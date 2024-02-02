39d1f775416a3f41536ccaadbb1b6b10d73b63a0
Chris Prabhu <chrisprabhu2@gmail.com>
2024-02-01 17:30:32 -0800

    login created

--- ShoppingApp.py
+++ ShoppingApp.py
@@ -1,7 +1,23 @@
-users = [
-    {
-        "name" : "Chris",
-        "type" : "admin"
+users = [{
+        'name': 'chris',
+        'type': 'admin'
     },
-    { "name": 'sarah', 'type' : 'customer'}]
-]
+    {
+        'name': 'sarah',
+        'type': 'customer'
+    }]
+
+user_name = input('Please enter username: ')
+user_type = ''
+
+for user in users:
+    if user['name'] == user_name:
+        user_type = user['type']
+        print('found user: ' + user_name + ' of type: ' + user_type)
+        break
+
+if not user_type:
+    print('user not found')
+    exit()
+
+
